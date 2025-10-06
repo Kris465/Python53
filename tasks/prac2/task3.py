75dff7e6b6d55909aa8c1ff070249cc063a588f8
Kris465 <nataza7394@gmail.com>
2025-10-06 19:08:32 +0300

    third task is done

---
+++ tasks/prac2/task3.py
@@ -0,0 +1,8 @@
+num1 = input("Введите первое число: ")
+num2 = input("Введите второе число: ")
+
+concan_result = num1 + num2
+sum_result = int(num1) + int(num2)
+
+print(f"Результат конкатенации: {concan_result}")
+print(f"Результат сложения: {sum_result}")
