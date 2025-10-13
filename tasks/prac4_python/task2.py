e53a9572b2b7537c4d01b0d6c27a05b5a83d22b6
Kris465 <nataza7394@gmail.com>
2025-10-13 20:50:46 +0300

    second task for prac4

--- tasks/prac4_python/task2.py
+++ tasks/prac4_python/task2.py
@@ -1,14 +1,16 @@
 mark = int(input("Введите балл: "))
-
-if mark >= 90:
-    grade = "A"
-elif mark >= 80:
-    grade = "B"
-elif mark >= 70:
-    grade = "C"
-elif mark >= 60:
-    grade = "D"
+if mark < 100 and mark > 0:
+    if mark >= 90:
+        grade = "A"
+    elif mark >= 80:
+        grade = "B"
+    elif mark >= 70:
+        grade = "C"
+    elif mark >= 60:
+        grade = "D"
+    else:
+        grade = "F"
 else:
-    grade = "F"
+    print("Вы ошиблись!")
 
 print(f"Оценка: {grade}")
