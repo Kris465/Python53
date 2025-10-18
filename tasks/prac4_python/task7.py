bcb42b2b929e1a926cf42e531826b8ec5861edb5
Kris465 <nataza7394@gmail.com>
2025-10-19 00:02:10 +0300

    7 python task is done

---
+++ tasks/prac4_python/task7.py
@@ -0,0 +1,8 @@
+year = int(input("Введите год: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} - високосный год")
+else:
+    print(f"{year} - не високосный год")
+
+# https://ru.wikipedia.org/wiki/%D0%92%D0%B8%D1%81%D0%BE%D0%BA%D0%BE%D1%81%D0%BD%D1%8B%D0%B9_%D0%B3%D0%BE%D0%B4
