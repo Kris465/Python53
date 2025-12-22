244923f17e84b3c6391c3fe2a38fe6fc1335c132
Kris465 <nataza7394@gmail.com>
2025-12-22 21:03:45 +0300

    task22

---
+++ sbornik/block1/task22.py
@@ -0,0 +1,8 @@
+x = int(input("Введите x "))
+a = int(input("Введите a "))
+
+y = 7 * x * x - 3 * x + 6
+x = 12 * a * a + 7 * a - 16
+
+print(f"7 * x * x - 3 * x + 6 = {y}, при x = {x}")
+print(f"12 * a * a + 7 * a - 16 = {x}, при a = {a}")
