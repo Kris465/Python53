00bb6772102111e0ba7cbc453d46385eef1366a4
Kris465 <nataza7394@gmail.com>
2026-01-26 19:56:29 +0300

    task8.py

---
+++ sbornik/block2/task8.py
@@ -0,0 +1,17 @@
+k = int(input("Введите число: "))
+if k == 1:
+    print("Понедльник")
+elif k == 2:
+    print("Вторник")
+elif k == 3:
+    print("Среда")
+elif k == 4:
+    print("Четверг")
+elif k == 5:
+    print("Pyatnica")
+elif k == 6:
+    print("Sybota")
+elif k == 7:
+    print("Voskresenie")
+else:
+    print("Net notogo dnya")
