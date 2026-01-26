f8edd303c68ba6d82e73cfb534f0bdda4e678edb
Kris465 <nataza7394@gmail.com>
2026-01-26 21:07:46 +0300

    task10.py

--- sbornik/block2/task10.py
+++ sbornik/block2/task10.py
@@ -1,6 +1,7 @@
 number = int(input("Введите n: "))
 
-desytki = number //10
+desytki = number // 10
 edinici = number % 10
 
 print(f" десятки = {desytki} , единицы = {edinici}")
+print(f" {desytki + edinici}, {desytki *edinici} ")
