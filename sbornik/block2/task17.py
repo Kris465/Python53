dfc90991ecccbd52ed51f31e0497306c6b90d84b
Kris465 <nataza7394@gmail.com>
2026-03-05 19:25:04 +0300

    task17.py

---
+++ sbornik/block2/task17.py
@@ -0,0 +1,9 @@
+number = int(input("Ведите трёхзначное число: "))
+
+num1 = number // 100
+print(num1)
+num2 = (number // 10) % 10
+print(num2)
+num3 = number % 10
+print(num3)
+print(num1, num3, num2)
